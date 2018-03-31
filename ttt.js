c33d372b0289df793c2da2a89538e86f36ee4df1
Megan <megan@Megans-MBP.lan>
2018-03-31 16:18:58 +1100

    further changes to tictactoe

--- ttt.js
+++ ttt.js
@@ -19,7 +19,6 @@ We'll be making tic tac toe (knots and crosses).
   7. Points on scoreboard for winning player
 */
 
-
 var cellOne = document.querySelector(".cellone")
 var cellTwo = document.querySelector(".celltwo")
 var cellThree = document.querySelector(".cellthree")
@@ -29,6 +28,8 @@ var cellSix = document.querySelector(".cellsix")
 var cellSeven = document.querySelector(".cellseven")
 var cellEight = document.querySelector(".celleight")
 var cellNine = document.querySelector(".cellnine")
+
+var allCells = [cellOne, cellTwo, cellThree, cellFour, cellFive, cellSix, cellSeven, cellEight, cellNine]
 
 var playerOneScore = document.querySelector(".playerOneScore")
 var playerTwoScore = document.querySelector(".playerTwoScore")
@@ -46,27 +47,55 @@ if (currentPlayer === 1) {
   playerOneLogo.classList.add('playerturn');
 }
 
-//When box 1 is clicked
-var revealCellOne = function () {
+//When cell is clicked
+var revealCell = function (event) {
 
   //Select specific classlist
-  EventTarget = document.querySelector('.cellone').classList
+  cell = event.target  // cell/ element where the click event happened
+  cellClass = cell.classList[1]
   //pushing clicked target into the array
 
-  if (cellOne.textContent === "X" || cellOne.textContent === "O") {
+  if (cell.textContent === "X" || cell.textContent === "O") {
     alert("The cell is already marked")
   } else if (currentPlayer === 1) {
-    cellOne.textContent = "X"
-    playerOneMoves.push(EventTarget[1])
+    cell.textContent = "X"
+    playerOneMoves.push(cellClass)
 
   } else if (currentPlayer === 2) {
-    cellOne.textContent = "O"
-    playerTwoMoves.push(EventTarget[1])
+    cell.textContent = "O"
+    playerTwoMoves.push(cellClass)
   }
 
   console.log(playerOneMoves)
   console.log(playerTwoMoves)
-  // switchPlayer()
+}
+
+//resetting the game after wins
+//clearing all cells and player arrays
+function resetGame() {
+  for (var i = 0; i < allCells.length; i++) {
+    var cell = allCells[i]
+    cell.textContent = ""
+  }
+  playerOneMoves = []
+  playerTwoMoves = []
+}
+
+//checking the game for draws
+//If so, apply the reset function at the end of the game
+function checkDraw() {
+  var hasEmptyCells = false;
+  for (var i = 0; i < allCells.length; i++) {
+    var cell = allCells[i]
+    if (cell.textContent == "") {
+      hasEmptyCells = true
+    }
+  }
+
+  if (hasEmptyCells == false) {
+    alert("Its a draw")
+    resetGame();
+  }
 }
 
 // check for wins
@@ -81,9 +110,6 @@ var winningCombos = [
   ['row-1box-3', 'row-2box-2', 'row-3box-1']
 ];
 
-//var playerOneMoves = []
-//var playerTwoMoves = []
-
 var countPlayerOneWins = 0;
 var countPlayerTwoWins = 0;
 
@@ -95,8 +121,9 @@ function checkWin() {
       && (playerOneMoves.indexOf(combo[1]) > -1)
       && (playerOneMoves.indexOf(combo[2]) > -1)) {
       alert("Player One Wins!")
+      resetGame();
 
-      //count win for Player One
+      //count wins for Player One
       countPlayerOneWins = countPlayerOneWins + 1
 
       // DISPLAY COUNT WINS FOR PLAYER ONE
@@ -106,24 +133,29 @@ function checkWin() {
       && (playerTwoMoves.indexOf(combo[1]) > -1)
       && (playerTwoMoves.indexOf(combo[2]) > -1)) {
       alert("Player Two Wins!")
+      resetGame();
 
-      //Count win for Player Two
+      //Count wins for Player Two
       countPlayerTwoWins = countPlayerTwoWins + 1
 
       //DISPLAY COUNT WINS FOR PLAYER TWO
       playerTwoScore.textContent = Number(playerTwoScore.textContent) + Number(countPlayerTwoWins)
+    } else {
+      checkDraw()
     }
-  }
+  } 
 }
 
-var switchPlayer = function () {
+//Switch between players
+//Once player one clicks, changes to player two's turn
 
-  //Once player one clicks, changes to player two's turn
+var switchPlayer = function () {
 
   if (currentPlayer === 1) {
     currentPlayer = 2
     playerTwoLogo.classList.add('playerturn');
     playerOneLogo.classList.remove('playerturn');
+
   } else if (currentPlayer === 2) {
     currentPlayer = 1
     playerTwoLogo.classList.remove('playerturn');
@@ -131,356 +163,15 @@ var switchPlayer = function () {
   }
 }
 
-
-
-cellOne.addEventListener('click', function () {
-  revealCellOne()
-  checkWin()
+//When cell is clicked, the following functions run
+function handleCellClick(event) {
+  revealCell(event)
   switchPlayer()
-})
-
-
-
-
-//When box 2 is clicked
-var revealCellTwo = function () {
-
-  EventTarget = document.querySelector('.celltwo').classList
-  //pushing clicked target into the array
-
-  if (cellTwo.textContent === "X" || cellTwo.textContent === "O") {
-    alert("The cell is already marked")
-  } else if (currentPlayer === 1) {
-    cellTwo.textContent = "X"
-    playerOneMoves.push(EventTarget[1])
-
-  } else if (currentPlayer === 2) {
-    cellTwo.textContent = "O"
-    playerTwoMoves.push(EventTarget[1])
-  }
-
-  console.log(playerOneMoves)
-  console.log(playerTwoMoves)
-
-}
-
-var switchPlayer = function () {
-
-  //Once player one clicks, changes to player two's turn
-  if (currentPlayer === 1) {
-    currentPlayer = 2
-    playerTwoLogo.classList.add('playerturn');
-    playerOneLogo.classList.remove('playerturn');
-  } else if (currentPlayer === 2) {
-    currentPlayer = 1
-    playerTwoLogo.classList.remove('playerturn');
-    playerOneLogo.classList.add('playerturn');
-  }
-}
-
-cellTwo.addEventListener('click', function () {
-  revealCellTwo()
   checkWin()
-  switchPlayer()
-})
-
-
-
-//When box 3 is clicked
-var revealCellThree = function () {
-
-  EventTarget = document.querySelector('.cellthree').classList
-  //pushing clicked target into the array
-
-  if (cellThree.textContent === "X" || cellThree.textContent === "O") {
-    alert("The cell is already marked")
-  } else if (currentPlayer === 1) {
-    cellThree.textContent = "X"
-
-    playerOneMoves.push(EventTarget[1])
-
-  } else if (currentPlayer === 2) {
-    cellThree.textContent = "O"
-    playerTwoMoves.push(EventTarget[1])
-  }
-
-  console.log(playerOneMoves)
-  console.log(playerTwoMoves)
 }
 
-var switchPlayer = function () {
-
-  //Once player one clicks, changes to player two's turn
-  if (currentPlayer === 1) {
-    currentPlayer = 2
-    playerTwoLogo.classList.add('playerturn');
-    playerOneLogo.classList.remove('playerturn');
-  } else if (currentPlayer === 2) {
-    currentPlayer = 1
-    playerTwoLogo.classList.remove('playerturn');
-    playerOneLogo.classList.add('playerturn');
-  }
+//add click event to all cells
+for (var i = 0; i < allCells.length; i++) {
+  var cell = allCells[i]
+  cell.addEventListener('click', handleCellClick)
 }
-
-
-cellThree.addEventListener('click', function () {
-  revealCellThree()
-  checkWin()
-  switchPlayer()
-});
-
-
-
-
-//When box 4 is clicked
-var revealCellFour = function () {
-
-  EventTarget = document.querySelector('.cellfour').classList
-  //pushing clicked target into the array
-
-  if (cellFour.textContent === "X" || cellFour.textContent === "O") {
-    alert("The cell is already marked")
-  } else if (currentPlayer === 1) {
-    cellFour.textContent = "X"
-    playerOneMoves.push(EventTarget[1])
-
-  } else if (currentPlayer === 2) {
-    cellFour.textContent = "O"
-    playerTwoMoves.push(EventTarget[1])
-  }
-
-  console.log(playerOneMoves)
-  console.log(playerTwoMoves)
-}
-
-var switchPlayer = function () {
-
-  //Once player one clicks, changes to player two's turn
-  if (currentPlayer === 1) {
-    currentPlayer = 2
-    playerTwoLogo.classList.add('playerturn');
-    playerOneLogo.classList.remove('playerturn');
-  } else if (currentPlayer === 2) {
-    currentPlayer = 1
-    playerTwoLogo.classList.remove('playerturn');
-    playerOneLogo.classList.add('playerturn');
-  }
-}
-
-cellFour.addEventListener('click', function () {
-  revealCellFour()
-  checkWin()
-  switchPlayer()
-})
-
-
-
-//When box 5 is clicked
-var revealCellFive = function () {
-
-  EventTarget = document.querySelector('.cellfive').classList
-  //pushing clicked target into the array
-
-  if (cellFive.textContent === "X" || cellFive.textContent === "O") {
-    alert("The cell is already marked")
-  } else if (currentPlayer === 1) {
-    cellFive.textContent = "X"
-
-    playerOneMoves.push(EventTarget[1])
-
-  } else if (currentPlayer === 2) {
-    cellFive.textContent = "O"
-    playerTwoMoves.push(EventTarget[1])
-  }
-
-  console.log(playerOneMoves)
-  console.log(playerTwoMoves)
-}
-
-var switchPlayer = function () {
-
-  //Once player one clicks, changes to player two's turn
-  if (currentPlayer === 1) {
-    currentPlayer = 2
-    playerTwoLogo.classList.add('playerturn');
-    playerOneLogo.classList.remove('playerturn');
-  } else if (currentPlayer === 2) {
-    currentPlayer = 1
-    playerTwoLogo.classList.remove('playerturn');
-    playerOneLogo.classList.add('playerturn');
-  }
-}
-
-cellFive.addEventListener('click', function () {
-  revealCellFive()
-  checkWin()
-  switchPlayer()
-})
-
-
-//When box 6 is clicked
-var revealCellSix = function () {
-
-  EventTarget = document.querySelector('.cellsix').classList
-  //pushing clicked target into the array
-
-  if (cellSix.textContent === "X" || cellSix.textContent === "O") {
-    alert("The cell is already marked")
-  } else if (currentPlayer === 1) {
-    cellSix.textContent = "X"
-    playerOneMoves.push(EventTarget[1])
-
-  } else if (currentPlayer === 2) {
-    cellSix.textContent = "O"
-    playerTwoMoves.push(EventTarget[1])
-  }
-
-  console.log(playerOneMoves)
-  console.log(playerTwoMoves)
-}
-
-
-var switchPlayer = function () {
-
-  //Once player one clicks, changes to player two's turn
-  if (currentPlayer === 1) {
-    currentPlayer = 2
-    playerTwoLogo.classList.add('playerturn');
-    playerOneLogo.classList.remove('playerturn');
-  } else if (currentPlayer === 2) {
-    currentPlayer = 1
-    playerTwoLogo.classList.remove('playerturn');
-    playerOneLogo.classList.add('playerturn');
-  }
-}
-
-cellSix.addEventListener('click', function () {
-  revealCellSix()
-  checkWin()
-  switchPlayer()
-})
-
-
-
-//When box 7 is clicked
-var revealCellSeven = function () {
-
-  EventTarget = document.querySelector('.cellseven').classList
-  //pushing clicked target into the array
-
-  if (cellSeven.textContent === "X" || cellSeven.textContent === "O") {
-    alert("The cell is already marked")
-  } else if (currentPlayer === 1) {
-    cellSeven.textContent = "X"
-    playerOneMoves.push(EventTarget[1])
-
-  } else if (currentPlayer === 2) {
-    cellSeven.textContent = "O"
-    playerTwoMoves.push(EventTarget[1])
-  }
-
-  console.log(playerOneMoves)
-  console.log(playerTwoMoves)
-}
-
-var switchPlayer = function () {
-
-  //Once player one clicks, changes to player two's turn
-  if (currentPlayer === 1) {
-    currentPlayer = 2
-
-  } else if (currentPlayer === 2) {
-    currentPlayer = 1
-  }
-}
-cellSeven.addEventListener('click', function () {
-  revealCellSeven()
-  checkWin()
-  switchPlayer()
-})
-
-
-
-//When box 8 is clicked
-var revealCellEight = function () {
-
-  EventTarget = document.querySelector('.celleight').classList
-  //pushing clicked target into the array
-
-  if (cellEight.textContent === "X" || cellEight.textContent === "O") {
-    alert("The cell is already marked")
-  } else if (currentPlayer === 1) {
-    cellEight.textContent = "X"
-    playerOneMoves.push(EventTarget[1])
-
-  } else if (currentPlayer === 2) {
-    cellEight.textContent = "O"
-    playerTwoMoves.push(EventTarget[1])
-  }
-
-  console.log(playerOneMoves)
-  console.log(playerTwoMoves)
-}
-
-var switchPlayer = function () {
-
-  //Once player one clicks, changes to player two's turn
-  if (currentPlayer === 1) {
-    currentPlayer = 2
-    playerTwoLogo.classList.add('playerturn');
-    playerOneLogo.classList.remove('playerturn');
-  } else if (currentPlayer === 2) {
-    currentPlayer = 1
-    playerTwoLogo.classList.remove('playerturn');
-    playerOneLogo.classList.add('playerturn');
-  }
-}
-
-cellEight.addEventListener('click', function () {
-  revealCellEight()
-  checkWin()
-  switchPlayer()
-})
-
-
-
-//When box 9 is clicked
-var revealCellNine = function () {
-
-  EventTarget = document.querySelector('.cellnine').classList
-  //pushing clicked target into the array
-
-  if (cellNine.textContent === "X" || cellNine.textContent === "O") {
-    alert("The cell is already marked")
-  } else if (currentPlayer === 1) {
-    cellNine.textContent = "X"
-    playerOneMoves.push(EventTarget[1])
-
-  } else if (currentPlayer === 2) {
-    cellNine.textContent = "O"
-    playerTwoMoves.push(EventTarget[1])
-  }
-
-  console.log(playerOneMoves)
-  console.log(playerTwoMoves)
-}
-
-var switchPlayer = function () {
-
-  //Once player one clicks, changes to player two's turn
-  if (currentPlayer === 1) {
-    currentPlayer = 2
-    playerTwoLogo.classList.add('playerturn');
-    playerOneLogo.classList.remove('playerturn');
-  } else if (currentPlayer === 2) {
-    currentPlayer = 1
-    playerTwoLogo.classList.remove('playerturn');
-    playerOneLogo.classList.add('playerturn');
-  }
-}
-
-cellNine.addEventListener('click', function () {
-  revealCellNine()
-  checkWin()
-  switchPlayer()
-})
